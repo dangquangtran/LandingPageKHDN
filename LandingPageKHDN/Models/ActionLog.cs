7096fcf0163af42bc3675d3e9b92b9f56fea07b6
Maverick2708 <daigiakien02@gmail.com>
2025-05-19 16:17:05 +0700

    Connect Database

---
+++ LandingPageKHDN/Models/ActionLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LandingPageKHDN.Models;
+
+public partial class ActionLog
+{
+    public int Id { get; set; }
+
+    public int AdminId { get; set; }
+
+    public string Action { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual AdminAccount Admin { get; set; } = null!;
+}
