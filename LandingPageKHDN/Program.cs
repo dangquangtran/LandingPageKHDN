5aae8a7b531b6e1b69adff255e6ee97d648bfd1c
Maverick2708 <daigiakien02@gmail.com>
2025-05-20 10:09:44 +0700

    Update appsetting.json

--- LandingPageKHDN/Program.cs
+++ LandingPageKHDN/Program.cs
@@ -1,9 +1,16 @@
+using LandingPageKHDN.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//Dbcontext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
