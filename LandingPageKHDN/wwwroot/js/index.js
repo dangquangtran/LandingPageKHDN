53b6975af454c0b0aab1f7535cb525f0bd9c474d
QuangLaTui <quangtran2002qn@gmail.com>
2025-05-21 13:36:49 +0700

    uu dai

---
+++ LandingPageKHDN/wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    function onScroll() {
+        var el = document.querySelector('.overview');
+        if (!el) return;
+        var rect = el.getBoundingClientRect();
+        var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+        if (rect.top <= windowHeight - 100) {
+            el.classList.add('show');
+            window.removeEventListener('scroll', onScroll);
+        }
+    }
+window.addEventListener('scroll', onScroll);
+});
