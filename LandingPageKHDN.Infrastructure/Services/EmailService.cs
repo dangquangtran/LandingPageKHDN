3f4628aa3c3041661f3b2e00daad3f7f5796fd04
Maverick2708 <daigiakien02@gmail.com>
2025-05-22 14:01:25 +0700

    Update Recaptcha

--- LandingPageKHDN.Infrastructure/Services/EmailService.cs
+++ LandingPageKHDN.Infrastructure/Services/EmailService.cs
@@ -8,7 +8,7 @@ using System.Net.Mail;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using LandingPageKHDN.Infrastructure.Templates.Email;
+//using LandingPageKHDN.Infrastructure.Templates.Email;
 
 namespace LandingPageKHDN.Infrastructure.Services
 {
