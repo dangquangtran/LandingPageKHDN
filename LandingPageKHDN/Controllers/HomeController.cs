75347c2a1f88561cbdd5e09b4019982721315bf0
QuangLaTui <quangtran2002qn@gmail.com>
2025-05-22 14:50:07 +0700

    recaptcha

--- LandingPageKHDN/Controllers/HomeController.cs
+++ LandingPageKHDN/Controllers/HomeController.cs
@@ -6,14 +6,17 @@ namespace LandingPageKHDN.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            ViewBag.RecaptchaSiteKey = _configuration["Recaptcha:SiteKey"];
             return View();
         }
 
